b4825bfb5745705e0e1cd5f3effac2d926cbf279
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2022-07-22 17:41:35 +0200

    glued SDK and met main dll's

--- SDK/SDK/Program.cs
+++ SDK/SDK/Program.cs
@@ -1,6 +1,8 @@
-﻿var manifest = new Home.HomeManifest();
+﻿using MV.OneD.UI;
+
+var manifest = new Home.HomeManifest();
 
 
-MV.Client.MVClient c = new MV.Client.MVClient();
-await c.Init(manifest.Definition());
+var app = new App();
+await app.Start(manifest);
 
